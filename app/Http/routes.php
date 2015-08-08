6c13504fb455241c398851e57b1dc739acbfd89c
den <gdlrb@mail.ru>
2015-08-08 23:16:04 +0300

    u1

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -14,6 +14,7 @@
 Route::get('/', 'WelcomeController@index');
 Route::get('contact', 'WelcomeController@contact');
 Route::get('about', 'PagesController@about');
+Route::get('contact', 'PagesController@contact');
 
 Route::get('home', 'HomeController@index');
 
